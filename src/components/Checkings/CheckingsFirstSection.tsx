53bcc749b485ca8df639b9264e7b13a82bab915d
Godwin Opara <godwinopara62@yahoo.com>
2024-06-28 10:46:24 +0100

    finished checking mobile design

--- src/components/Checkings/CheckingsFirstSection.tsx
+++ src/components/Checkings/CheckingsFirstSection.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function CheckingsFirstSection() {
   return (
     <section className="py-10">
-      <h2 className="text-center text-4xl font-light mb-10">Your choice of three checking accounts</h2>
-      <div className="grid grid-cols-3 container">
-        <div className="border-r border-gray2 p-10">
+      <h2 className="text-center text-2xl xl:text-4xl font-light mb-10">Your choice of three checking accounts</h2>
+      <div className="grid xl:grid-cols-3 container">
+        <div className="border-b xl:border-b-0 xl:border-r border-gray2 p-10 mb-10 xl:mb-0">
           <div className="mb-8">
             <Image
               src="https://www.firsthorizon.com/-/media/Images/Icons/General/SVGs/FHBIcons/FHB-DebitCreditCardWide.svg"
@@ -20,7 +20,7 @@ export default function CheckingsFirstSection() {
             You like to keep things simple, skip the bells and whistles, and just get your business done.
           </p>
         </div>
-        <div className="border-r border-gray2 p-10">
+        <div className="border-b xl:border-b-0 xl:border-r border-gray2 p-10 mb-10 xl:mb-0">
           <div className="mb-5">
             <Image
               src="https://www.firsthorizon.com/-/media/Images/Icons/General/SVGs/FHBIcons/FHB-DebitCreditCardPlus.svg"
--- src/components/Checkings/CheckingsHero.tsx
+++ src/components/Checkings/CheckingsHero.tsx
@@ -1,11 +1,11 @@
 export default function CheckingsHero() {
   return (
-    <section className="bg-checkingHero bg-no-repeat min-h-[65vh] bg-center bg-cover grid place-content-center">
-      <div className="container  text-white grid grid-cols-2 gap-x-32">
-        <div className="bg-white text-secondary pl-12 pr-24 py-10 min-w-[750px] flex flex-col justify-center">
-          <h1 className="text-4xl mb-5">You’re not one size fits all.</h1>
+    <section className="bg-checkingHero bg-no-repeat min-h-[50vh] xl:min-h-[65vh] bg-right xl:bg-center xl:bg-cover grid place-content-center">
+      <div className="container text-white xl:grid xl:grid-cols-2 xl:gap-x-32">
+        <div className="bg-white text-secondary px-10 xl:pl-12 xl:pr-24 py-10 xl:min-w-[750px] flex flex-col justify-center">
+          <h1 className="text-2xl xl:text-4xl mb-5">You’re not one size fits all.</h1>
           <p className="mb-5">So why is your checking account?</p>
-          <button className="uppercase bg-primary text-white w-[280px] h-[50px] text-sm font-medium tracking-widest">
+          <button className="uppercase bg-primary text-white xl:w-[280px] h-[50px] text-sm font-medium tracking-widest">
             {"I'd"} like a recommendation
           </button>
         </div>
